726379bf0d08bd4758f8e36d3f26df5cf58f511b
karololszak <k.olszacki@timecamp.com>
2018-12-06 12:09:18 +0100

    allow for context menu and refresh options

--- src/Overrides/TCWebEngineView.h
+++ src/Overrides/TCWebEngineView.h
@@ -14,6 +14,7 @@ public:
 
 protected:
     QWebEngineView *createWindow(QWebEnginePage::WebWindowType type) override;
+    void contextMenuEvent(QContextMenuEvent *event) override;
 
 private:
     TCWebEngineView *result;
